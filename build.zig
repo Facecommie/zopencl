637abf7ab786f531c4012825b74351d896263fcc
Nasser West <nasserwest11@gmail.com>
2025-07-23 22:56:26 +0100

    fixing some things

--- build.zig
+++ build.zig
@@ -1,21 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const headers = b.dependency("opencl_headers", .{});
-
-    const opencl = b.addModule("opencl", .{
-        .root_source_file = b.path("src/opencl.zig"),
-        .link_libc = true,
-        .target = target,
-        .optimize = optimize,
+    _ = b.addModule("root", .{
+        .root_source_file = b.path("src/zopencl.zig"),
     });
-    opencl.addIncludePath(headers.path(""));
-    // TODO: Instead of relying on a system package,
-    // we should either package the OpenCL-ICD-Loader
-    // with Zig or manually load function pointers
-    // from the OpenCL library.
-    opencl.linkSystemLibrary("OpenCL", .{});
 }
